ec1a999a644b2825ce6a797899e3e74971e52b37
Nandhini0615 <nandhinithisan06@gmail.com>
2024-09-04 23:35:07 +0000

    lesson 1 done

---
+++ lesson.py
@@ -0,0 +1,15 @@
+# name = input("What's your name?").upper()
+# print("Hello" + name + ".")
+# print(f"hello, {name}.")?
+
+def greet(name):
+    print(f"Hello, {name}")
+
+greet ("Nandhy")
+greet ("praveen")
+
+Username = input("enter your username: ")
+greet("username")
+
+# num1 =int(input("number 1:"))
+# num2 =int(input("number 2:"))
